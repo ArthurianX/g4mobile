03024c1bc817f18ba64911e33c7b6f4a62e7ccf8
Kovacs Arthur Kraid <kovacs.arthur.kraid@gmail.com>
2019-02-21 09:12:08 +0200

    Making android work.

--- android/app/src/main/java/com/karthurk/g4mobile/MainActivity.java
+++ android/app/src/main/java/com/karthurk/g4mobile/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "G4 Media Mobile";
+        return "G4Mobile";
     }
 }
